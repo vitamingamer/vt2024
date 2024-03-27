cdbae3cef3c188a856b090809d34d389f3990f71
vitamingamer <axel.cristenson@elev.ga.ntig.se>
2024-03-27 15:11:19 +0100

    Inlämning
    Ok

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-// Denna fil skall innehålla JavaScript (JS)
+const ball = document.querySelector("#ball")
+const troll = document.querySelector("#troll")
+
+ball.addEventListener('click', changeColor)
+troll.addEventListener('click', vanish)
+
+function changeColor() {
+    ball.classList.toggle("active");
+}   
+function vanish() {
+    troll.classList.toggle("active")
+}
